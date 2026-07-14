3d98995552adbbabf9e189e5f798527a4ecf8658
Daniel Heid <mail@daniel-heid.de>
2026-07-14 14:57:35 +0200

    Add additional unit tests for `Micropolis` class properties and power functionality

--- src/test/java/micropolisj/engine/MicropolisTest.java
+++ src/test/java/micropolisj/engine/MicropolisTest.java
@@ -1,12 +1,34 @@
 package micropolisj.engine;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-
 import org.junit.jupiter.api.Test;
+
+import static micropolisj.engine.TileConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class MicropolisTest {
 
   private final Micropolis micropolis = new Micropolis();
+
+  @Test
+  public void testInitialization() {
+    assertThat(micropolis.getWidth()).isEqualTo(120);
+    assertThat(micropolis.getHeight()).isEqualTo(100);
+    assertThat(micropolis.getTile(0, 0)).isEqualTo(DIRT);
+  }
+
+  @Test
+  public void testSetAndGetTile() {
+    micropolis.setTile(5, 5, FIRE);
+    assertThat(micropolis.getTile(5, 5)).isEqualTo(FIRE);
+  }
+
+  @Test
+  public void testPower() {
+    micropolis.setTile(5, 5, POWERPLANT);
+    micropolis.setTilePower(5, 5, true);
+    assertThat(micropolis.isTilePowered(5, 5)).isTrue();
+  }
 
   @Test
   public void testMakeFire() {
